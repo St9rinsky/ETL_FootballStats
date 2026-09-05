63bc453e9e10cdd57b4f72f406beac4ef7dc7a82
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-09-05 06:01:00 +0200

    fix: solved ouput path inconsistencies, broke down date column from uct to date and time column, matches can either be finished or schedules for status

--- src/transform/matches.py
+++ src/transform/matches.py
@@ -1,28 +1,30 @@
+from pathlib import Path
 from pyspark.sql import SparkSession
 from delta.tables import DeltaTable
-from pyspark.sql.functions import col
-
+from pyspark.sql.functions import col, to_timestamp, to_date, date_format, when
 
 def transform_matches(spark: SparkSession, matches_data, output_path):
-
-    table_path = str(output_path) + "matches"
 
     matches = matches_data.select(
         col("match.id").alias("match_id"),
         col("match.matchday").alias("match_day"),
-        col("match.utcDate").alias("match_date"),
-        col("match.status").alias("status"),
+
+        date_format(to_timestamp(col("match.utcDate")),"yyyy-MM-dd").alias("match_date"),
+
+        date_format(to_timestamp(col("match.utcDate")),"HH:mm").alias("match_time"),
+
+        when(col("match.status") != "FINISHED","SCHEDULED")
+        .otherwise(col("match.status")).alias("status"),
+
         col("match.homeTeam.id").alias("home_team_id"),
-        col("match.homeTeam.shortName").alias("home_team"),
         col("match.awayTeam.id").alias("away_team_id"),
-        col("match.awayTeam.shortName").alias("away_team"),
         col("match.score.fullTime.home").alias("home_goals"),
         col("match.score.fullTime.away").alias("away_goals")
     )
 
-    if DeltaTable.isDeltaTable(spark, table_path):
+    if DeltaTable.isDeltaTable(spark, output_path):
 
-        existing_table = DeltaTable.forPath(spark, table_path)
+        existing_table = DeltaTable.forPath(spark, output_path)
 
         existing_table.alias("old")\
         .merge(matches.alias("new"),"old.match_id = new.match_id")\
@@ -31,4 +33,4 @@ def transform_matches(spark: SparkSession, matches_data, output_path):
         .execute()
 
     else:
-        matches.write.format("delta").mode("overwrite").save(table_path)
+        matches.write.format("delta").mode("overwrite").save(output_path)
